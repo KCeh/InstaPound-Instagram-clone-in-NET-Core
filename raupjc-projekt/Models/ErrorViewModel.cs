c51698fdc941f382b2d450060027aea79212f0c3
Karlo Čeh <karlo.ceh@fer.hr>
2017-12-29 13:19:18 +0100

    inital commit

---
+++ raupjc-projekt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace raupjc_projekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
